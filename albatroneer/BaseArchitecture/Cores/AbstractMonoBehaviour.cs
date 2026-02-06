d6fdab24fdec660ef67371d7fce048c2ca75db4c
albert <strannoo1@gmail.com>
2026-02-06 10:15:46 +0500

    first commit

---
+++ albatroneer/BaseArchitecture/Cores/AbstractMonoBehaviour.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+namespace albatroneer.BaseArchitecture.Cores
+{
+    /// <summary>
+    /// custom MonoBehaviour with need functions, not use Awake/Start. Only Init()
+    /// </summary>
+    public abstract class AbstractMonoBehaviour : MonoBehaviour
+    {
+        [Range(0, 100)]
+        [SerializeField] private int _priority;
+
+        public int Priority => _priority;
+
+        protected bool _isInit = false;
+        
+
+        [field: SerializeField] public bool IsInject { get; private set; } = true; 
+
+        /// <summary>
+        /// Not Use, Use Init()
+        /// </summary>
+        protected void Awake()
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Not Use, Use Init()
+        /// </summary>
+        protected void Start()
+        {
+            return;
+        }
+
+        public void TryInit()
+        {
+            if (_isInit)
+            {
+                return;
+            }
+
+            Init();
+            
+            _isInit = true;
+        }
+
+        protected abstract void Init();
+
+        protected void OnDestroy()
+        {
+            Dispose();
+        }
+
+        protected virtual void Dispose()
+        {
+            
+        }
+
+    }
+}
+
