5ac5ad16e4e97f1a6d5812eb8640339ff6171bc8
albert <strannoo1@gmail.com>
2026-02-06 10:32:07 +0500

    update

---
+++ BaseArchitecture/Events/IEventer.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+namespace albatroneer.BaseArchitecture.Events
+{
+    public interface IEventer
+    {
+        public void Subscribe<T>(Action<T> callback, int priority = 0) where T : struct;
+        public void Unsubscribe<T>(Action<T> callback) where T : struct;
+        public void Publish<T>(T eventData) where T : struct;
+        
+        
+        public void Subscribe<T>(Action<T> callback, Guid guid, int priority = 0) where T : struct;
+        public void Unsubscribe<T>(Action<T> callback , Guid guid) where T : struct;
+        public void Publish<T>(T eventData , Guid guid) where T : struct;
+
+        public void SubscribeGlobal<T>(Action<T> callback, int priority = 0) where T : struct;
+        
+        public void UnsubscribeAllGuid(Guid guid);
+
+    }
+
+}
